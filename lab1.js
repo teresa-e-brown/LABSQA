f5d35222ade0a26e3af6404fd1249fb4430b0d60
teresa-e-brown <teresa_e_brown@hotmail.com>
2024-02-06 16:21:41 +0000

    commit1

---
+++ lab1.js
@@ -0,0 +1,11 @@
+//alert ("ok");
+
+    var answer;
+    answer = confirm('Are you sure?');
+
+    if (answer == false) {
+        alert ('Cancel pressed');
+    }
+    else {
+        alert ('OK pressed');
+    }
